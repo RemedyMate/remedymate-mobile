1f44eecdfdf10a183f2a311c1861c38a21db3d90
Gemechu Alemu <alemugemechu44@gmail.com>
2025-09-04 15:33:00 +0300

    home page to chat page navigation  icon is solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'app.dart';
 import 'dependency_injection.dart' as di;
 import 'features/chatbot/domain/usecases/start_chat_usecase.dart';
 import 'features/chatbot/presentation/bloc/chatbot_bloc.dart';
-import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
